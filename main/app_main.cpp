de45c8df8d8b5a00fc49240f2d01066254ce5000
Daniel <dndanielski@gmail.com>
2024-05-05 20:39:42 -0230

    Update app-main

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -1,9 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
 #include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
